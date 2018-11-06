64489e4cdd06fb387d9b877737b86cf96c82a939
cfernando415 <ca.fernando415@gmail.com>
2018-11-06 16:22:44 -0500

    finished exercise

--- solution.rb
+++ solution.rb
@@ -1,1 +1,19 @@
+def palindrome?(arg)
+  i = arg.length - 1
+  str = ""
+  while i >= 0
+    str += arg[i]
+    i -= 1
+  end
+  if str == arg
+    true
+  else
+    false
+  end
+end
 
+puts palindrome?("racecar")
+puts palindrome?("noon")
+puts palindrome?("five")
+puts palindrome?("radar")
+puts palindrome?("learnlovecode")
