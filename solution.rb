aabfffec05703fa2da926ebb04b02e78989ebed3
Brian Horowitz <guitarrook94@gmail.com>
2018-11-06 16:44:34 -0500

    Solutions to palindrome lab

--- solution.rb
+++ solution.rb
@@ -1,1 +1,28 @@
+require 'pry'
 
+def palindrome(arg)
+  a = arg
+  if a.reverse == a
+    return true
+  else
+    return false
+  end
+end
+
+def palindrome2?(arg)
+  a = arg
+  if a[0..99].reverse == a
+    return true
+  else
+    return false
+  end
+end
+#0
+
+# def palindrome3?(arg)
+#   a = arg
+#   if a[0] == a[-1] && a[2] == a[-3]
+# end
+
+
+binding.pry
